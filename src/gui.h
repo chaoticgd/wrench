3c9ca44bb492c2c6d5186981517839340b83002f
chaoticgd <chaoticgd@protonmail.com>
2019-06-19 00:36:07 +0100

    Moved imgui to thirdparty/

--- src/gui.h
+++ src/gui.h
@@ -20,11 +20,10 @@
 #define GUI_H
 
 #include <functional>
-
-#include "imgui/imgui.h"
-#include "imgui/misc/cpp/imgui_stdlib.h"
-#include "imgui/examples/imgui_impl_glfw.h"
-#include "imgui/examples/imgui_impl_opengl3.h"
+#include <imgui/imgui.h>
+#include <imgui/misc/cpp/imgui_stdlib.h>
+#include <imgui/examples/imgui_impl_glfw.h>
+#include <imgui/examples/imgui_impl_opengl3.h>
 
 #include "app.h"
 #include "tool.h"
