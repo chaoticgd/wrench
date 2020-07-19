ffa7c0b0595aa193a6dc62b83557120a05e3a26c
chaoticgd <chaoticgd@protonmail.com>
2020-07-19 17:49:04 +0100

    Use linear filtering for textures

--- src/formats/texture.cpp
+++ src/formats/texture.cpp
@@ -104,8 +104,8 @@ void texture::upload_to_opengl() {
 	glGenTextures(1, &_opengl_id);
 	glBindTexture(GL_TEXTURE_2D, _opengl_id);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, _size.x, _size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, colour_data.data());
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 }
 
 GLuint texture::opengl_id() const {
