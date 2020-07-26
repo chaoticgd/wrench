177609a7cdcce9fcafdf92811b90ca10d88e97c8
chaoticgd <chaoticgd@protonmail.com>
2020-07-26 15:45:51 +0100

    models: Make an ASCII bracket look more l33t
    Also this silences a compiler warning.

--- src/formats/game_model.cpp
+++ src/formats/game_model.cpp
@@ -176,7 +176,7 @@ std::vector<moby_subsubmodel> moby_model::read_subsubmodels(
 			if(start_index != i) {
 				moby_subsubmodel subsubmodel;
 				// Iterate over one maximal contiguous list of non-zero indices.
-				//                  /-----^-----\ 
+				//                  .-----^-----.
 				// indices = { 0x0, 0x1, 0x2, 0x3, 0x0, 0x4, ... }
 				for(std::size_t j = start_index + 1; j < i; j++) {
 					// Unravel the tristrip into a regular GL_TRIANGLES index
