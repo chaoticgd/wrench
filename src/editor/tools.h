13bc25d53cfe1c50f0e2151991ac81d6719d2188
chaoticgd <chaoticgd@protonmail.com>
2021-07-28 00:38:29 +0100

    editor: Get rid of the spline tool for now

--- src/editor/tools.h
+++ src/editor/tools.h
@@ -70,16 +70,4 @@ private:
 	glm::vec3 _displacement;
 };
 
-class spline_tool : public tool {
-public:
-	spline_tool();
-	
-	void draw(app& a, glm::mat4 world_to_clip) override;
-
-private:
-	InstanceId _selected_spline = NULL_INSTANCE_ID;
-	size_t _selected_vertex = 0;
-	glm::vec3 _plane_normal { 0.f, 0.f, 1.f };
-};
-
 #endif
