b72491faf0e20dc48cf6e6d3a42e243b198a390f
chaoticgd <chaoticgd@protonmail.com>
2021-08-21 19:26:35 +0100

    wad: Fix building R&C1 levels

--- src/wad/wad_file.cpp
+++ src/wad/wad_file.cpp
@@ -217,10 +217,10 @@ static std::vector<u8> build_level_wad(LevelWad& wad) {
 			Rac1LevelWadHeader header = {0};
 			header.header_size = sizeof(Rac1LevelWadHeader);
 			header.level_number = wad.level_number;
-			dest.alloc<Rac23LevelWadHeader>();
+			dest.alloc<Rac1LevelWadHeader>();
 			header.primary = write_primary(dest, wad);
 			wad.help_messages.swap(wad.gameplay); // help_messages -> gameplay
-			std::vector<u8> gameplay = write_gameplay(wad, wad.gameplay, wad.game, RAC23_GAMEPLAY_BLOCKS);
+			std::vector<u8> gameplay = write_gameplay(wad, wad.gameplay, wad.game, RAC1_GAMEPLAY_BLOCKS);
 			wad.help_messages.swap(wad.gameplay); // gameplay -> help_messages
 			header.gameplay_ntsc = write_compressed_lump(dest, gameplay);
 			header.occlusion = write_lump(dest, write_occlusion(wad.gameplay, wad.game));
