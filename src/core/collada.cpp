d4c1819720169fc9b38a472517b971a00812f15e
chaoticgd <chaoticgd@protonmail.com>
2021-11-05 22:31:34 +0000

    wad: Add missing license header, remove unused function

--- src/core/collada.cpp
+++ src/core/collada.cpp
@@ -1,3 +1,20 @@
+/*
+	wrench - A set of modding tools for the Ratchet & Clank PS2 games.
+	Copyright (C) 2019-2021 chaoticgd
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include "collada.h"
 
@@ -7,12 +24,6 @@ using XmlNode = rapidxml::xml_node<>;
 using XmlAttrib = rapidxml::xml_attribute<>;
 #define xml_for_each_child_of_type(child, parent, name) \
 	for(const XmlNode* child = parent->first_node(name); child != nullptr; child = child->next_sibling(name))
-
-ColladaScene mesh_to_dae(Mesh mesh) {
-	ColladaScene scene;
-	scene.meshes.emplace_back(std::move(mesh));
-	return scene;
-}
 
 using IdMap = std::map<std::string, const XmlNode*>;
 using NodeToIndexMap = std::map<const XmlNode*, s32>;
