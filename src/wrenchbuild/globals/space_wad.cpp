e71e3e0ffce9caf45fe7306bd8fe157941d9e955
chaoticgd <>
2022-06-03 16:13:21 +0100

    build: Start reversing the UYA space wad

--- src/wrenchbuild/globals/space_wad.cpp
+++ src/wrenchbuild/globals/space_wad.cpp
@@ -19,25 +19,53 @@
 #include <wrenchbuild/asset_unpacker.h>
 #include <wrenchbuild/asset_packer.h>
 
+packed_struct(UyaSpaceWadHeader,
+	/* 0x000 */ s32 header_size;
+	/* 0x004 */ Sector32 sector;
+	/* 0x008 */ SectorRange stuff[6];
+	/* 0x038 */ SectorRange wads[9];
+	/* 0x080 */ Sector32 vags_1[2];
+	/* 0x088 */ SectorRange subtitles;
+	/* 0x090 */ Sector32 vags_2[10];
+	/* 0x0b8 */ Sector32 wads_2[69];
+	/* 0x1cc */ Sector32 vags_3[14];
+	/* 0x204 */ Sector32 wads_3[83];
+	/* 0x350 */ Sector32 wads_4[484];
+	/* 0xae0 */ SectorRange moby_classes[42];
+)
+static_assert(sizeof(UyaSpaceWadHeader) == 0xc30);
+
 packed_struct(DlSpaceWadHeader,
 	/* 0x0 */ s32 header_size;
 	/* 0x4 */ Sector32 sector;
 	/* 0x8 */ SectorRange transition_wads[12];
 )
 
-void unpack_space_wad(SpaceWadAsset& dest, const DlSpaceWadHeader& header, InputStream& src, Game game);
-static void pack_space_wad(OutputStream& dest, DlSpaceWadHeader& header, const SpaceWadAsset& src, Game game);
+void unpack_uya_space_wad(SpaceWadAsset& dest, const UyaSpaceWadHeader& header, InputStream& src, Game game);
+static void pack_uya_space_wad(OutputStream& dest, UyaSpaceWadHeader& header, const SpaceWadAsset& src, Game game);
+void unpack_dl_space_wad(SpaceWadAsset& dest, const DlSpaceWadHeader& header, InputStream& src, Game game);
+static void pack_dl_space_wad(OutputStream& dest, DlSpaceWadHeader& header, const SpaceWadAsset& src, Game game);
 
 on_load(Space, []() {
-	SpaceWadAsset::funcs.unpack_dl = wrap_wad_unpacker_func<SpaceWadAsset, DlSpaceWadHeader>(unpack_space_wad);
+	SpaceWadAsset::funcs.unpack_rac3 = wrap_wad_unpacker_func<SpaceWadAsset, UyaSpaceWadHeader>(unpack_uya_space_wad);
+	SpaceWadAsset::funcs.unpack_dl = wrap_wad_unpacker_func<SpaceWadAsset, DlSpaceWadHeader>(unpack_dl_space_wad);
 	
-	SpaceWadAsset::funcs.pack_dl = wrap_wad_packer_func<SpaceWadAsset, DlSpaceWadHeader>(pack_space_wad);
+	SpaceWadAsset::funcs.pack_rac3 = wrap_wad_packer_func<SpaceWadAsset, UyaSpaceWadHeader>(pack_uya_space_wad);
+	SpaceWadAsset::funcs.pack_dl = wrap_wad_packer_func<SpaceWadAsset, DlSpaceWadHeader>(pack_dl_space_wad);
 })
 
-void unpack_space_wad(SpaceWadAsset& dest, const DlSpaceWadHeader& header, InputStream& src, Game game) {
+void unpack_uya_space_wad(SpaceWadAsset& dest, const UyaSpaceWadHeader& header, InputStream& src, Game game) {
+	
+}
+
+static void pack_uya_space_wad(OutputStream& dest, UyaSpaceWadHeader& header, const SpaceWadAsset& src, Game game) {
+	
+}
+
+void unpack_dl_space_wad(SpaceWadAsset& dest, const DlSpaceWadHeader& header, InputStream& src, Game game) {
 	unpack_compressed_assets<BinaryAsset>(dest.transitions(), src, ARRAY_PAIR(header.transition_wads), game);
 }
 
-static void pack_space_wad(OutputStream& dest, DlSpaceWadHeader& header, const SpaceWadAsset& src, Game game) {
+static void pack_dl_space_wad(OutputStream& dest, DlSpaceWadHeader& header, const SpaceWadAsset& src, Game game) {
 	pack_compressed_assets_sa(dest, ARRAY_PAIR(header.transition_wads), src.get_transitions(), game, "trnsition");
 }
