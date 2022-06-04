b6bcc7351a0d41705ae8b4ce461f23823478ebdc
chaoticgd <>
2022-06-04 14:43:36 +0100

    build: Fix packing/unpacking the IRX wad

--- src/wrenchbuild/globals/irx_wad.cpp
+++ src/wrenchbuild/globals/irx_wad.cpp
@@ -19,138 +19,138 @@
 #include <wrenchbuild/asset_unpacker.h>
 #include <wrenchbuild/asset_packer.h>
 
-packed_struct(GcIrxModules,
-	/* 0x00 */ ByteRange sio2man;
-	/* 0x08 */ ByteRange mcman;
-	/* 0x10 */ ByteRange mcserv;
-	/* 0x18 */ ByteRange padman;
-	/* 0x20 */ ByteRange mtapman;
-	/* 0x28 */ ByteRange libsd;
-	/* 0x30 */ ByteRange _989snd;
-)
-
-packed_struct(UyaIrxModules,
-	/* 0x00 */ ByteRange stash;
-	/* 0x08 */ ByteRange inet;
-	/* 0x10 */ ByteRange netcnf;
-	/* 0x18 */ ByteRange inetctl;
-	/* 0x20 */ ByteRange msifrpc;
-	/* 0x28 */ ByteRange dev9;
-	/* 0x30 */ ByteRange smap;
-	/* 0x38 */ ByteRange libnetb;
-	/* 0x40 */ ByteRange ppp;
-	/* 0x48 */ ByteRange pppoe;
-	/* 0x50 */ ByteRange usbd;
-	/* 0x58 */ ByteRange lgaud;
-	/* 0x60 */ ByteRange eznetcnf;
-	/* 0x68 */ ByteRange eznetctl;
-	/* 0x70 */ ByteRange lgkbm;
-)
-
-packed_struct(DlIrxModules,
-	/* 0x0 */ ByteRange streamer;
-	/* 0x8 */ ByteRange astrm;
-)
-
 packed_struct(GcIrxHeader,
 	/* 0x00 */ ByteRange image;
 	/* 0x04 */ ByteRange unused[2];
-	/* 0x18 */ GcIrxModules rac2;
+	/* 0x18 */ ByteRange sio2man;
+	/* 0x20 */ ByteRange mcman;
+	/* 0x28 */ ByteRange mcserv;
+	/* 0x30 */ ByteRange dbcman;
+	/* 0x38 */ ByteRange sio2d;
+	/* 0x40 */ ByteRange ds2u;
+	/* 0x48 */ ByteRange stash;
+	/* 0x50 */ ByteRange libsd;
+	/* 0x58 */ ByteRange _989snd;
 )
 
 packed_struct(UyaIrxHeader,
 	/* 0x00 */ s32 unused[2];
-	/* 0x08 */ GcIrxModules rac2;
-	/* 0x40 */ UyaIrxModules rac3;
+	/* 0x08 */ ByteRange sio2man;
+	/* 0x10 */ ByteRange mcman;
+	/* 0x18 */ ByteRange mcserv;
+	/* 0x20 */ ByteRange padman;
+	/* 0x28 */ ByteRange mtapman;
+	/* 0x30 */ ByteRange libsd;
+	/* 0x38 */ ByteRange _989snd;
+	/* 0x40 */ ByteRange stash;
+	/* 0x48 */ ByteRange inet;
+	/* 0x50 */ ByteRange netcnf;
+	/* 0x58 */ ByteRange inetctl;
+	/* 0x60 */ ByteRange msifrpc;
+	/* 0x68 */ ByteRange dev9;
+	/* 0x70 */ ByteRange smap;
+	/* 0x78 */ ByteRange libnetb;
+	/* 0x80 */ ByteRange ppp;
+	/* 0x88 */ ByteRange pppoe;
+	/* 0x90 */ ByteRange usbd;
+	/* 0x98 */ ByteRange lgaud;
+	/* 0xa0 */ ByteRange eznetcnf;
+	/* 0xa8 */ ByteRange eznetctl;
+	/* 0xb0 */ ByteRange lgkbm;
 )
 
 packed_struct(DlIrxHeader,
 	/* 0x00 */ s32 unused[2];
-	/* 0x08 */ GcIrxModules rac2;
-	/* 0x40 */ UyaIrxModules rac3;
-	/* 0xb8 */ DlIrxModules dl;
+	/* 0x08 */ ByteRange sio2man;
+	/* 0x10 */ ByteRange mcman;
+	/* 0x18 */ ByteRange mcserv;
+	/* 0x20 */ ByteRange padman;
+	/* 0x28 */ ByteRange mtapman;
+	/* 0x30 */ ByteRange libsd;
+	/* 0x38 */ ByteRange _989snd;
+	/* 0x40 */ ByteRange stash;
+	/* 0x48 */ ByteRange inet;
+	/* 0x50 */ ByteRange netcnf;
+	/* 0x58 */ ByteRange inetctl;
+	/* 0x60 */ ByteRange msifrpc;
+	/* 0x68 */ ByteRange dev9;
+	/* 0x70 */ ByteRange smap;
+	/* 0x78 */ ByteRange libnetb;
+	/* 0x80 */ ByteRange ppp;
+	/* 0x88 */ ByteRange pppoe;
+	/* 0x90 */ ByteRange usbd;
+	/* 0x98 */ ByteRange lgaud;
+	/* 0xa0 */ ByteRange eznetcnf;
+	/* 0xa8 */ ByteRange eznetctl;
+	/* 0xb0 */ ByteRange lgkbm;
+	/* 0xb8 */ ByteRange streamer;
+	/* 0xc0 */ ByteRange astrm;
 )
 
-static void unpack_rac1_irx_wad(IrxWadAsset& dest, InputStream& src, Game game);
-static void pack_rac1_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game);
-static void unpack_rac2_irx_wad(IrxWadAsset& dest, InputStream& src, Game game);
-static void pack_rac2_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game);
-static void unpack_rac3_irx_wad(IrxWadAsset& dest, InputStream& src, Game game);
-static void pack_rac3_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game);
-static void unpack_dl_irx_wad(IrxWadAsset& dest, InputStream& src, Game game);
-static void pack_dl_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game);
-static void unpack_rac2_irx_modules(IrxWadAsset& dest, const GcIrxModules& header, InputStream& src, Game game);
-static GcIrxModules pack_rac2_irx_modules(OutputStream& dest, const IrxWadAsset& src, Game game);
-static void unpack_rac3_irx_modules(IrxWadAsset& dest, const UyaIrxModules& header, InputStream& src, Game game);
-static UyaIrxModules pack_rac3_irx_modules(OutputStream& dest, const IrxWadAsset& src, Game game);
-static void unpack_dl_irx_modules(IrxWadAsset& dest, const DlIrxModules& header, InputStream& src, Game game);
-static DlIrxModules pack_dl_irx_modules(OutputStream& dest, const IrxWadAsset& src, Game game);
+static void unpack_rac_irx_wad(IrxWadAsset& dest, InputStream& src, Game game);
+static void pack_rac_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game);
+static void unpack_gc_irx_wad(IrxWadAsset& dest, InputStream& src, Game game);
+static void pack_gc_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game);
+static void unpack_uya_irx_wad(IrxWadAsset& dest, InputStream& src, Game game);
+static void pack_uya_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game);
+template <typename Header>
+static void unpack_uya_dl_irx_wad(IrxWadAsset& dest, InputStream& src, Game game);
+template <typename Header>
+static void pack_uya_dl_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game);
 
 on_load(Irx, []() {
-	IrxWadAsset::funcs.unpack_rac1 = wrap_unpacker_func<IrxWadAsset>(unpack_rac1_irx_wad);
-	IrxWadAsset::funcs.unpack_rac2 = wrap_unpacker_func<IrxWadAsset>(unpack_rac2_irx_wad);
-	IrxWadAsset::funcs.unpack_rac3 = wrap_unpacker_func<IrxWadAsset>(unpack_rac3_irx_wad);
-	IrxWadAsset::funcs.unpack_dl = wrap_unpacker_func<IrxWadAsset>(unpack_dl_irx_wad);
+	IrxWadAsset::funcs.unpack_rac1 = wrap_unpacker_func<IrxWadAsset>(unpack_rac_irx_wad);
+	IrxWadAsset::funcs.unpack_rac2 = wrap_unpacker_func<IrxWadAsset>(unpack_gc_irx_wad);
+	IrxWadAsset::funcs.unpack_rac3 = wrap_unpacker_func<IrxWadAsset>(unpack_uya_dl_irx_wad<UyaIrxHeader>);
+	IrxWadAsset::funcs.unpack_dl = wrap_unpacker_func<IrxWadAsset>(unpack_uya_dl_irx_wad<DlIrxHeader>);
 	
-	IrxWadAsset::funcs.pack_rac1 = wrap_packer_func<IrxWadAsset>(pack_rac1_irx_wad);
-	IrxWadAsset::funcs.pack_rac2 = wrap_packer_func<IrxWadAsset>(pack_rac2_irx_wad);
-	IrxWadAsset::funcs.pack_rac3 = wrap_packer_func<IrxWadAsset>(pack_rac3_irx_wad);
-	IrxWadAsset::funcs.pack_dl = wrap_packer_func<IrxWadAsset>(pack_dl_irx_wad);
+	IrxWadAsset::funcs.pack_rac1 = wrap_packer_func<IrxWadAsset>(pack_rac_irx_wad);
+	IrxWadAsset::funcs.pack_rac2 = wrap_packer_func<IrxWadAsset>(pack_gc_irx_wad);
+	IrxWadAsset::funcs.pack_rac3 = wrap_packer_func<IrxWadAsset>(pack_uya_dl_irx_wad<UyaIrxHeader>);
+	IrxWadAsset::funcs.pack_dl = wrap_packer_func<IrxWadAsset>(pack_uya_dl_irx_wad<DlIrxHeader>);
 })
 
-static void unpack_rac1_irx_wad(IrxWadAsset& dest, InputStream& src, Game game) {
+static void unpack_rac_irx_wad(IrxWadAsset& dest, InputStream& src, Game game) {
 	verify_not_reached("R&C1 IRX unpacking not yet implemented.");
 }
 
-static void pack_rac1_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game) {
+static void pack_rac_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game) {
 	verify_not_reached("R&C1 IRX packing not yet implemented.");
 }
 
-static void unpack_rac2_irx_wad(IrxWadAsset& dest, InputStream& src, Game game) {
+static void unpack_gc_irx_wad(IrxWadAsset& dest, InputStream& src, Game game) {
 	auto header = src.read<GcIrxHeader>(0);
 	unpack_asset(dest.image<TextureAsset>(), src, header.image, game, FMT_TEXTURE_RGBA);
-	unpack_rac2_irx_modules(dest, header.rac2, src, game);
+	unpack_asset(dest.sio2man(), src, header.sio2man, game);
+	unpack_asset(dest.mcman(), src, header.mcman, game);
+	unpack_asset(dest.mcserv(), src, header.mcserv, game);
+	unpack_asset(dest.dbcman(), src, header.dbcman, game);
+	unpack_asset(dest.sio2d(), src, header.sio2d, game);
+	unpack_asset(dest.ds2u(), src, header.ds2u, game);
+	unpack_asset(dest.stash(), src, header.stash, game);
+	unpack_asset(dest.libsd(), src, header.libsd, game);
+	unpack_asset(dest._989snd(), src, header._989snd, game);
 }
 
-static void pack_rac2_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game) {
+static void pack_gc_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game) {
 	GcIrxHeader header = {0};
 	dest.write(header);
 	header.image = pack_asset<ByteRange>(dest, src.get_image(), game, 0x40, FMT_TEXTURE_RGBA);
-	header.rac2 = pack_rac2_irx_modules(dest, src, game);
+	header.sio2man = pack_asset<ByteRange>(dest, src.get_sio2man(), game, 0x40);
+	header.mcman = pack_asset<ByteRange>(dest, src.get_mcman(), game, 0x40);
+	header.mcserv = pack_asset<ByteRange>(dest, src.get_mcserv(), game, 0x40);
+	header.dbcman = pack_asset<ByteRange>(dest, src.get_dbcman(), game, 0x40);
+	header.sio2d = pack_asset<ByteRange>(dest, src.get_sio2d(), game, 0x40);
+	header.ds2u = pack_asset<ByteRange>(dest, src.get_ds2u(), game, 0x40);
+	header.stash = pack_asset<ByteRange>(dest, src.get_stash(), game, 0x40);
+	header.libsd = pack_asset<ByteRange>(dest, src.get_libsd(), game, 0x40);
+	header._989snd = pack_asset<ByteRange>(dest, src.get_989snd(), game, 0x40);
 	dest.write(0, header);
 }
 
-static void unpack_rac3_irx_wad(IrxWadAsset& dest, InputStream& src, Game game) {
-	auto header = src.read<UyaIrxHeader>(0);
-	unpack_rac2_irx_modules(dest, header.rac2, src, game);
-	unpack_rac3_irx_modules(dest, header.rac3, src, game);
-}
-
-static void pack_rac3_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game) {
-	UyaIrxHeader header = {0};
-	dest.write(header);
-	header.rac2 = pack_rac2_irx_modules(dest, src, game);
-	header.rac3 = pack_rac3_irx_modules(dest, src, game);
-	dest.write(0, header);
-}
-
-static void unpack_dl_irx_wad(IrxWadAsset& dest, InputStream& src, Game game) {
-	auto header = src.read<DlIrxHeader>(0);
-	unpack_rac2_irx_modules(dest, header.rac2, src, game);
-	unpack_rac3_irx_modules(dest, header.rac3, src, game);
-	unpack_dl_irx_modules(dest, header.dl, src, game);
-}
-
-static void pack_dl_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game) {
-	DlIrxHeader header = {0};
-	dest.write(header);
-	header.rac2 = pack_rac2_irx_modules(dest, src, game);
-	header.rac3 = pack_rac3_irx_modules(dest, src, game);
-	header.dl = pack_dl_irx_modules(dest, src, game);
-	dest.write(0, header);
-}
-
-static void unpack_rac2_irx_modules(IrxWadAsset& dest, const GcIrxModules& header, InputStream& src, Game game) {
+template <typename Header>
+static void unpack_uya_dl_irx_wad(IrxWadAsset& dest, InputStream& src, Game game) {
+	auto header = src.read<Header>(0);
 	unpack_asset(dest.sio2man(), src, header.sio2man, game);
 	unpack_asset(dest.mcman(), src, header.mcman, game);
 	unpack_asset(dest.mcserv(), src, header.mcserv, game);
@@ -158,21 +158,6 @@ static void unpack_rac2_irx_modules(IrxWadAsset& dest, const GcIrxModules& heade
 	unpack_asset(dest.mtapman(), src, header.mtapman, game);
 	unpack_asset(dest.libsd(), src, header.libsd, game);
 	unpack_asset(dest._989snd(), src, header._989snd, game);
-}
-
-static GcIrxModules pack_rac2_irx_modules(OutputStream& dest, const IrxWadAsset& src, Game game) {
-	GcIrxModules modules;
-	modules.sio2man = pack_asset<ByteRange>(dest, src.get_sio2man(), game, 0x40);
-	modules.mcman = pack_asset<ByteRange>(dest, src.get_mcman(), game, 0x40);
-	modules.mcserv = pack_asset<ByteRange>(dest, src.get_mcserv(), game, 0x40);
-	modules.padman = pack_asset<ByteRange>(dest, src.get_padman(), game, 0x40);
-	modules.mtapman = pack_asset<ByteRange>(dest, src.get_mtapman(), game, 0x40);
-	modules.libsd = pack_asset<ByteRange>(dest, src.get_libsd(), game, 0x40);
-	modules._989snd = pack_asset<ByteRange>(dest, src.get_989snd(), game, 0x40);
-	return modules;
-}
-
-static void unpack_rac3_irx_modules(IrxWadAsset& dest, const UyaIrxModules& header, InputStream& src, Game game) {
 	unpack_asset(dest.stash(), src, header.stash, game);
 	unpack_asset(dest.inet(), src, header.inet, game);
 	unpack_asset(dest.netcnf(), src, header.netcnf, game);
@@ -188,36 +173,41 @@ static void unpack_rac3_irx_modules(IrxWadAsset& dest, const UyaIrxModules& head
 	unpack_asset(dest.eznetcnf(), src, header.eznetcnf, game);
 	unpack_asset(dest.eznetctl(), src, header.eznetctl, game);
 	unpack_asset(dest.lgkbm(), src, header.lgkbm, game);
+	if constexpr(std::is_same_v<Header, DlIrxHeader>) {
+		unpack_asset(dest.streamer(), src, header.streamer, game);
+		unpack_asset(dest.astrm(), src, header.astrm, game);
+	}
 }
 
-static UyaIrxModules pack_rac3_irx_modules(OutputStream& dest, const IrxWadAsset& src, Game game) {
-	UyaIrxModules modules;
-	modules.stash = pack_asset<ByteRange>(dest, src.get_stash(), game, 0x40);
-	modules.inet = pack_asset<ByteRange>(dest, src.get_inet(), game, 0x40);
-	modules.netcnf = pack_asset<ByteRange>(dest, src.get_netcnf(), game, 0x40);
-	modules.inetctl = pack_asset<ByteRange>(dest, src.get_inetctl(), game, 0x40);
-	modules.msifrpc = pack_asset<ByteRange>(dest, src.get_msifrpc(), game, 0x40);
-	modules.dev9 = pack_asset<ByteRange>(dest, src.get_dev9(), game, 0x40);
-	modules.smap = pack_asset<ByteRange>(dest, src.get_smap(), game, 0x40);
-	modules.libnetb = pack_asset<ByteRange>(dest, src.get_libnetb(), game, 0x40);
-	modules.ppp = pack_asset<ByteRange>(dest, src.get_ppp(), game, 0x40);
-	modules.pppoe = pack_asset<ByteRange>(dest, src.get_pppoe(), game, 0x40);
-	modules.usbd = pack_asset<ByteRange>(dest, src.get_usbd(), game, 0x40);
-	modules.lgaud = pack_asset<ByteRange>(dest, src.get_lgaud(), game, 0x40);
-	modules.eznetcnf = pack_asset<ByteRange>(dest, src.get_eznetcnf(), game, 0x40);
-	modules.eznetctl = pack_asset<ByteRange>(dest, src.get_eznetctl(), game, 0x40);
-	modules.lgkbm = pack_asset<ByteRange>(dest, src.get_lgkbm(), game, 0x40);
-	return modules;
-}
-
-static void unpack_dl_irx_modules(IrxWadAsset& dest, const DlIrxModules& header, InputStream& src, Game game) {
-	unpack_asset(dest.streamer(), src, header.streamer, game);
-	unpack_asset(dest.astrm(), src, header.astrm, game);
-}
-
-static DlIrxModules pack_dl_irx_modules(OutputStream& dest, const IrxWadAsset& src, Game game) {
-	DlIrxModules modules;
-	modules.streamer = pack_asset<ByteRange>(dest, src.get_streamer(), game, 0x40);
-	modules.astrm = pack_asset<ByteRange>(dest, src.get_astrm(), game, 0x40);
-	return modules;
+template <typename Header>
+static void pack_uya_dl_irx_wad(OutputStream& dest, const IrxWadAsset& src, Game game) {
+	Header header = {0};
+	dest.write(header);
+	header.sio2man = pack_asset<ByteRange>(dest, src.get_sio2man(), game, 0x40);
+	header.mcman = pack_asset<ByteRange>(dest, src.get_mcman(), game, 0x40);
+	header.mcserv = pack_asset<ByteRange>(dest, src.get_mcserv(), game, 0x40);
+	header.padman = pack_asset<ByteRange>(dest, src.get_padman(), game, 0x40);
+	header.mtapman = pack_asset<ByteRange>(dest, src.get_mtapman(), game, 0x40);
+	header.libsd = pack_asset<ByteRange>(dest, src.get_libsd(), game, 0x40);
+	header._989snd = pack_asset<ByteRange>(dest, src.get_989snd(), game, 0x40);
+	header.stash = pack_asset<ByteRange>(dest, src.get_stash(), game, 0x40);
+	header.inet = pack_asset<ByteRange>(dest, src.get_inet(), game, 0x40);
+	header.netcnf = pack_asset<ByteRange>(dest, src.get_netcnf(), game, 0x40);
+	header.inetctl = pack_asset<ByteRange>(dest, src.get_inetctl(), game, 0x40);
+	header.msifrpc = pack_asset<ByteRange>(dest, src.get_msifrpc(), game, 0x40);
+	header.dev9 = pack_asset<ByteRange>(dest, src.get_dev9(), game, 0x40);
+	header.smap = pack_asset<ByteRange>(dest, src.get_smap(), game, 0x40);
+	header.libnetb = pack_asset<ByteRange>(dest, src.get_libnetb(), game, 0x40);
+	header.ppp = pack_asset<ByteRange>(dest, src.get_ppp(), game, 0x40);
+	header.pppoe = pack_asset<ByteRange>(dest, src.get_pppoe(), game, 0x40);
+	header.usbd = pack_asset<ByteRange>(dest, src.get_usbd(), game, 0x40);
+	header.lgaud = pack_asset<ByteRange>(dest, src.get_lgaud(), game, 0x40);
+	header.eznetcnf = pack_asset<ByteRange>(dest, src.get_eznetcnf(), game, 0x40);
+	header.eznetctl = pack_asset<ByteRange>(dest, src.get_eznetctl(), game, 0x40);
+	header.lgkbm = pack_asset<ByteRange>(dest, src.get_lgkbm(), game, 0x40);
+	if constexpr(std::is_same_v<Header, DlIrxHeader>) {
+		header.streamer = pack_asset<ByteRange>(dest, src.get_streamer(), game, 0x40);
+		header.astrm = pack_asset<ByteRange>(dest, src.get_astrm(), game, 0x40);
+	}
+	dest.write(0, header);
 }
