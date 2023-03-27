e408cca8bdf8e85caf34d14f6a049d78782d16c8
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2023-03-28 00:17:21 +0100

    Include glibc version in Linux release zip name

--- cmake/release_crafter.cmake
+++ cmake/release_crafter.cmake
@@ -8,7 +8,12 @@ if(ZIP_RELEASE)
 		set(RELEASE_VERSION ${GIT_TAG})
 	endif()
 	if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-		set(RELEASE_OS "linux")
+		execute_process(
+			COMMAND bash -c "ldd --version | sed -n 's/ldd (Ubuntu GLIBC //p' | sed 's/).*//'"
+			OUTPUT_VARIABLE GLIBC_VERSION_STRING
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		set(RELEASE_OS "linux-glibc${GLIBC_VERSION_STRING}")
 	elseif(APPLE)
 		set(RELEASE_OS "mac")
 	elseif(WIN32)
