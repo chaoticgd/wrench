ab1288f1aac6faafc29ef159d8c39560894fe258
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2023-06-08 08:10:34 +0100

    assetmgr: Fix a crash when a placeholder asset exists in the game bank, but not the underlay

--- src/assetmgr/asset.h
+++ src/assetmgr/asset.h
@@ -456,11 +456,11 @@ private:
 template <typename ClassAsset>
 std::string generate_asset_path(const char* directory, const char* type, s32 id, const Asset& parent) {
 	if(parent.has_child(id)) {
-		const ClassAsset& child = parent.get_child(id).as<ClassAsset>();
-		if(child.has_name() && !child.name().empty()) {
-			std::string name = to_snake_case(child.name().c_str());
-			if(child.has_category() && !child.category().empty()) {
-				std::string category = to_snake_case(child.category().c_str());
+		const ClassAsset* child = parent.get_child(id).maybe_as<ClassAsset>();
+		if(child && child->has_name() && !child->name().empty()) {
+			std::string name = to_snake_case(child->name().c_str());
+			if(child->has_category() && !child->category().empty()) {
+				std::string category = to_snake_case(child->category().c_str());
 				return stringf("%s/%s/%d_%s/%s_%s.asset", directory, category.c_str(), id, name.c_str(), type, name.c_str());
 			} else {
 				return stringf("%s/%d_%s/%s_%s.asset", directory, id, name.c_str(), type, name.c_str());
