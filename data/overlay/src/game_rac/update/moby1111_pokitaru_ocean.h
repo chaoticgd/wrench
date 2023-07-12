8519e8a7fcf75cdafdffb4475bdbf10eba1d4f93
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2023-07-12 21:15:59 +0100

    pvars: Add some R&C1 ocean moby classes

---
+++ data/overlay/src/game_rac/update/moby1111_pokitaru_ocean.h
@@ -0,0 +1,16 @@
+#pragma wrench parser on
+
+struct update1111 { // 0x30
+	/* 0x00 */ int unknown_0;
+	/* 0x04 */ int unknown_4;
+	/* 0x08 */ int unknown_8;
+	/* 0x0c */ int unknown_c;
+	/* 0x10 */ float unknown_10;
+	/* 0x14 */ float surfaceZ;
+	/* 0x18 */ float unknown_18;
+	/* 0x1c */ float unknown_1c;
+	/* 0x20 */ float unknown_20;
+	/* 0x24 */ float unknown_24;
+	/* 0x28 */ float unknown_28;
+	/* 0x2c */ float unknown_2c;
+};
